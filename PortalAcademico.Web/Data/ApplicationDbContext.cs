d67cc808febdd1d4f6eb8faba14b4aaaa43dbad3
kiaraLinares19 <kiaralinaresestivariz@gmail.com>
2025-10-04 03:48:33 -0500

    Data

--- PortalAcademico.Web/Data/ApplicationDbContext.cs
+++ PortalAcademico.Web/Data/ApplicationDbContext.cs
@@ -1,12 +1,39 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PortalAcademico.Web.Models;
 
-namespace PortalAcademico.Web.Data;
-
-public class ApplicationDbContext : IdentityDbContext
+namespace PortalAcademico.Web.Data
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Matricula> Matriculas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Curso>()
+                .HasIndex(c => c.Codigo)
+                .IsUnique();
+
+            builder.Entity<Matricula>()
+                .HasIndex(m => new { m.CursoId, m.UsuarioId })
+                .IsUnique(); // Un usuario no puede matricularse dos veces
+
+            builder.Entity<Curso>()
+                .Property(c => c.Creditos)
+                .IsRequired()
+                .HasDefaultValue(1);
+
+            builder.Entity<Curso>()
+                .HasCheckConstraint("CK_Curso_Creditos", "Creditos > 0");
+
+            builder.Entity<Curso>()
+                .HasCheckConstraint("CK_Curso_Horario", "HorarioInicio < HorarioFin");
+        }
     }
 }
